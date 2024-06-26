febc211312a7fd8ded0fa53b17c3d4d7c110e047
Nicolas Mayorga <namayorgav@gmail.com>
2024-06-25 21:56:39 -0500

    [ADD] All the logic behind the Form submission implementing the query to the API.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,25 +1,50 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (searchQuery.trim() !== "") {
+      navigate(`/items?search=${encodeURIComponent(searchQuery)}`)
+    }
+    event.currentTarget.reset()
+  }
+
   return (
-    <header role="searchbox" aria-label="search bar" className="searchBar">
+    <header className="searchBar">
       <div className="searchBar__container">
-        <picture className="searchBar__logo">
+        <a className="searchBar__logo" href="/">
           <img src="/assets/Logo_ML.png" alt="logo" />
-        </picture>
-        <form className="searchBar__form">
+        </a>
+        <form className="searchBar__form" onSubmit={handleSubmit}>
           <input
+            aria-label="search bar"
+            role="searchbox"
             className="searchBar__input"
-            type="text"
-            placeholder="Search"
+            type="search"
+            placeholder="Nunca dejes de buscar"
             maxLength={120}
             autoCapitalize="off"
             autoCorrect="off"
             autoComplete="off"
             spellCheck="false"
+            name="search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
           />
-          <button className="searchBar__subtmitBtn">Search</button>
+          <button className="searchBar__submitBtn" type="submit">
+            <img
+              src="/assets/ic_Search.png"
+              alt="Icono de busqueda de elementos"
+            />
+          </button>
         </form>
       </div>
     </header>
   )
 }
+
 export default SearchBar
