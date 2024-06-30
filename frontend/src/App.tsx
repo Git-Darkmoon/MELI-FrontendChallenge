da26a860a34e40efd40d9cc9d07d482d13125af2
Nicolas Mayorga <namayorgav@gmail.com>
2024-06-29 20:57:07 -0500

    [ADD] Not found page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import HomeLayout from "./pages/HomeLayout"
 import SearchResult from "./pages/SearchResult"
 import ProductDetails from "./pages/ProductDetails"
+import NotFound from "./pages/NotFound"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         index: true,
